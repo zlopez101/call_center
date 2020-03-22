c2e73328fa1a1ff9bfd551ac939dfd10199d855b
zlopez101 <zachlopez9@gmail.com>
2020-03-22 16:20:45 -0500

    more

--- UT/website/forms.py
+++ UT/website/forms.py
@@ -6,14 +6,9 @@ from wtforms.validators import DataRequired
 class SignUp(FlaskForm):
     first_name = StringField("First Name", validators=[DataRequired()])
     last_name = StringField("Last Name", validators=[DataRequired()])
-    Address = StringField("Address", validators=[DataRequired()])
-    Sex = SelectField(
-        "Sex",
-        validators=[DataRequired()],
-        coerce=int,
-        choices=[(1, "Male"), (2, "Female"), (3, "Other")],
-    )
-    date_of_birth = DateField("DOB", validators=[DataRequired()])
+    # Address = StringField("Address", validators=[DataRequired()])
+    date_of_birth = DateField("DOB", format="%m/%d/%Y", validators=[DataRequired()])
+    """
     martial_status = SelectField(
         "martial status",
         validators=[DataRequired()],
@@ -50,20 +45,26 @@ class SignUp(FlaskForm):
             (5, "Divorced"),
         ],
     )
+    """
     language = SelectField(
-        "martial status",
+        "Language",
         validators=[DataRequired()],
         coerce=int,
         choices=[
-            (1, "Single"),
-            (2, "Married"),
-            (3, "Widowed"),
-            (4, "Separated"),
-            (5, "Divorced"),
+            (1, "English"),
+            (2, "Spanish"),
+            (3, "French"),
+            (4, "Chinese"),
+            (5, "Korean"),
         ],
     )
-    City = StringField("City", validators=[DataRequired()])
-    Zip = StringField("Zip code", validators=[DataRequired()])
+    # City = StringField("City", validators=[DataRequired()])
+    # Zip = StringField("Zip code", validators=[DataRequired()])
     phone_number = StringField("Phone number", validators=[DataRequired()])
     email = StringField("Email Address", validators=[DataRequired()])
+    submit = SubmitField("Submit")
+
+
+class CheckApt(FlaskForm):
+    date = DateField("Date", format="%m/%d", validators=[DataRequired()])
     submit = SubmitField("Submit")
--- UT/website/routes.py
+++ UT/website/routes.py
@@ -1,6 +1,6 @@
-from flask import render_template, Blueprint, flash
+from flask import render_template, Blueprint, flash, redirect, url_for
 from ut.models import Location, Appointment
-from ut.website.forms import SignUp
+from ut.website.forms import SignUp, CheckApt
 
 website = Blueprint("website", __name__)
 
@@ -11,14 +11,42 @@ def home():
     return render_template("welcome.html", locations=locations)
 
 
-@website.route("/<int:locationid>")
+@website.route("/<int:locationid>", methods=["GET", "POST"])
 def samplelocation(locationid):
     appointments = Appointment.query.filter_by(location_id=locationid).all()
     location = Location.query.filter_by(id=locationid).first_or_404()
     appointments = []
-    form = SignUp()
+    form = CheckApt()
     if form.validate_on_submit():
         flash("We submitted a form", "success")
+        return redirect(
+            url_for(
+                "website.samplelocation_with_date",
+                locationid=location.id,
+                date=form.date.data,
+            )
+        )
     return render_template(
-        "location.html", title="title", appointments=appointments, location=location
+        "location.html",
+        title="title",
+        appointments=appointments,
+        location=location,
+        form=form,
+        legend="Pick a date to check appointment availability",
+    )
+
+
+@website.route("/<int:locationid>/<string:date>", methods=["GET", "POST"])
+def samplelocation_with_date(locationid, date):
+    appointments = Appointment.query.filter_by(schedule_date=date).all()
+    location = Location.query.filter_by(id=locationid).first_or_404()
+    form = SignUp()
+    if form.validate_on_submit():
+        flash("Still got it!")
+    return render_template(
+        "appointment.html",
+        title=f"{location.name} on {date} appointments",
+        legend=f"{location.name} on {date} appointments. Submit Form to sign into an appointment.",
+        form=form,
+        appointments=appointments,
     )
