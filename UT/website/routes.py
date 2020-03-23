93b6a0f6210b3b5528913b8ad430b7e0918822fb
zlopez101 <zachlopez9@gmail.com>
2020-03-23 00:20:27 -0500

    working on appointment table

--- UT/website/routes.py
+++ UT/website/routes.py
@@ -1,6 +1,9 @@
 from flask import render_template, Blueprint, flash, redirect, url_for
 from ut.models import Location, Appointment
 from ut.website.forms import SignUp, CheckApt
+from datetime import datetime
+import numpy as np
+
 
 website = Blueprint("website", __name__)
 
@@ -38,8 +41,12 @@ def samplelocation(locationid):
 
 @website.route("/<int:locationid>/<string:date>", methods=["GET", "POST"])
 def samplelocation_with_date(locationid, date):
+    date = datetime.strptime(date, "%Y-%m-%d").replace(2020)
     appointments = Appointment.query.filter_by(schedule_date=date).all()
     location = Location.query.filter_by(id=locationid).first_or_404()
+    dates = np.arange(date, date + 3, dtype="datetime64[D]")
+    print(type(date))
+    print(date)
     form = SignUp()
     if form.validate_on_submit():
         flash("Still got it!")
@@ -49,4 +56,5 @@ def samplelocation_with_date(locationid, date):
         legend=f"{location.name} on {date} appointments. Submit Form to sign into an appointment.",
         form=form,
         appointments=appointments,
+        dates=dates,
     )
