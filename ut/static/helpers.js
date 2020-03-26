0377aed958c912571e237a9b2c4e7e71ad29a01f
zlopez101 <zachlopez9@gmail.com>
2020-03-26 09:07:23 -0500

    more mapping, created user input still need to configure JS to render map and highligth closest two options

---
+++ ut/static/helpers.js
@@ -0,0 +1,6 @@
+const user_input = document.getElementById('addressInput')
+const bigWelcome = document.getElementById('bigWelcomeContainer')
+
+user_input.addEventListener('submit', function () {
+
+})
